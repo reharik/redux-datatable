cceadaac5bdf5b26b17bf849d7f7c174966d809e
Raif Harik <reharik@gmail.com>
2016-08-12 21:42:32 -0500

    fixing damn ... problem

--- src/utilities/initialize.js
+++ src/utilities/initialize.js
@@ -1,22 +1,27 @@
-import {updateConfigs } from './configValues';
+import {config, updateConfigs } from './configValues';
 import  fetchData from './fetchData';
 import { bulkSelectionColumn } from '../components/CheckBox';
 import {setColumns} from './../modules/columnModule';
 
 export default function(dispatch, ownProps) {
-  var config = updateConfigs(ownProps.config);
-  var columns = ownProps.columns;
+  if(config.initialized){ return; }
 
+  var newConfig = ownProps.config;
+  newConfig.initialized = true;
+  var _config = updateConfigs(newConfig);
+  var columns = ownProps.columns;
+  console.log('config');
+  console.log(_config);
   //TODO exptract into strategy or command pattern later
-  if(config.bulkSelection.mode !== 'none'){
-    columns.unshift(bulkSelectionColumn(config));
+  if(_config.bulkSelection.mode !== 'none'){
+    columns.unshift(bulkSelectionColumn(_config));
   }
   //TODO prolly do something like create default columns from data name here
   if(columns) {
     dispatch(setColumns(ownProps.columns))
   }
 
-  if(config.dataSource) {
-    fetchData(dispatch, config);
+  if(_config.dataSource) {
+    fetchData(dispatch, _config);
   }
 }
